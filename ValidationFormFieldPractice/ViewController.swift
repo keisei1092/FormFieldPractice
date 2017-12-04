6f8da7554d814b00f7b5caa1715206a397ff6a82
keisei1092 <keiseis1092@gmail.com>
2017-12-04 10:10:35 +0900

    Initial Commit

---
+++ ValidationFormFieldPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ValidationFormFieldPractice
+//
+//  Created by Keisei Saito on 2017/12/04.
+//  Copyright © 2017 Keisei Saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
