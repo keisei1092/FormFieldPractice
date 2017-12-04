fe78810b2f3cd275616a95e9edc0a66e7759f3ab
keisei1092 <keiseis1092@gmail.com>
2017-12-04 11:35:40 +0900

    validate it

---
+++ ValidationFormFieldPractice/FormTextField.swift
@@ -0,0 +1,40 @@
+//
+//  FormTextField.swift
+//  ValidationFormFieldPractice
+//
+//  Created by Keisei Saito on 2017/12/04.
+//  Copyright © 2017 Keisei Saito. All rights reserved.
+//
+
+import UIKit
+
+class FormTextField: UITextField {
+    private var validatorLayer: CALayer!
+
+    var isValid = true {
+        didSet {
+            validatorLayer.isHidden = isValid
+            textColor = isValid ? .black : .red
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        didInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        didInit()
+    }
+
+    private func didInit() {
+        // バリデーション用の下線
+        validatorLayer = CALayer()
+        validatorLayer.backgroundColor = UIColor.red.cgColor
+        validatorLayer.frame = CGRect(x: 0, y: self.frame.height - 2, width: self.frame.width, height: 2)
+        validatorLayer.isHidden = true
+        layer.addSublayer(validatorLayer)
+    }
+
+}
--- ValidationFormFieldPractice/ViewController.swift
+++ ValidationFormFieldPractice/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: FormTextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textField.addTarget(self, action: #selector(handleTextField(_:)), for: .editingChanged)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func handleTextField(_ textField: FormTextField) {
+        // バリデーション例として20文字以内であるかチェック
+        guard let text = textField.text else { return }
+        textField.isValid = text.characters.count < 20
+    }
 
 }
 
